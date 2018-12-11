a75a45e8377803e39d22dd295d094e0a147eba52
trashcluster <33961608+trashcluster@users.noreply.github.com>
2018-12-11 10:16:40 +0100

    Initial commit

---
+++ Level 1/Affichage de texte,suite d'instructions/Empilement de cylindres/main.py
@@ -0,0 +1,22 @@
+#include "robot.h"
+
+using namespace std;
+
+int main()
+{
+    deplacer(1,2);
+    deplacer(1,3);
+    deplacer(2,3);
+    deplacer(1,2);
+    deplacer(3,1);
+    deplacer(3,2);
+    deplacer(1,2);
+    deplacer(1,3);
+    deplacer(2,3);
+    deplacer(2,1);
+    deplacer(3,1);
+    deplacer(2,3);
+    deplacer(1,2);
+    deplacer(1,3);
+    deplacer(2,3);
+}
